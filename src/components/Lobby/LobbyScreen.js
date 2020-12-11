dcfc71587411bbf01eb23ba67bea32b008115fe2
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-12-11 13:17:37 -0800

    start modals in lobby
    Co-authored-by: Chris Hamersly <christopherhamersly@gmail.com>
    Co-authored-by: Tia Low <lowtia@gmail.com>

--- src/components/Lobby/LobbyScreen.js
+++ src/components/Lobby/LobbyScreen.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import { View, Text, Pressable } from 'react-native'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-native'
+import React from 'react';
+import { View, Text, Pressable, TouchableHighlight } from 'react-native';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-native';
+import JoinGame from './Modals/JoinGame';
+import PrivateGame from './Modals/PrivateGame';
+import StartGame from './Modals/StartGame';
 
-function StartScreen (props) {
+function StartScreen(props) {
 
   return (
     <View>
-      <Text> Welcome {props.userName}! </Text> 
+      <Text> Welcome {props.userName}! </Text>
       <Text> Short explanation of options below </Text>
-      <Text> Start a Game </Text>
-      <Text> Join a Game </Text>
-      <Text> Join Private Game </Text>
+      <TouchableHighlight>
+        <Text> Start a Game </Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text> Join a Game </Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text> Join Private Game </Text>
+      </TouchableHighlight>
     </View>
   )
 }
