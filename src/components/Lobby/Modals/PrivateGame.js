20843c42cc7504154f20a83d10ba408b2481df2a
Tia Low <lowtia@gmail.com>
2020-12-17 15:20:32 -0800

    Join private game modal functionality, collecting game code in state
      Co-authored-by: Chris Hamersly <christopherhamersly@gmail.com>
    Co-authored-by: Josh Williams <joshuasrwilliams@gmail.com>

--- src/components/Lobby/Modals/PrivateGame.js
+++ src/components/Lobby/Modals/PrivateGame.js
@@ -1,8 +1,18 @@
-import React from 'react'
-import { View, Text, Modal, Pressable } from 'react-native'
-import styles from '../../../styles/styles'
+import React, { useState } from 'react'
+import { View, Text, Modal, Pressable, TextInput } from 'react-native'
+import { Input } from 'react-native-elements';
+import { Link } from 'react-router-native';
+import styles from '../../../styles/styles';
 
 function PrivateGame(props) {
+
+  const [ gameCode, setGameCode ]= useState('')
+
+
+
+
+
+
   return (
     <Modal
         transparent={true}
@@ -12,6 +22,23 @@ function PrivateGame(props) {
             style={styles.modalView}
           >
             <Text>JOIN a private game here!!</Text>
+            <Text>Enter Code</Text>
+
+
+            <Input
+              placeholder={'code here'}
+              style={styles.input}
+              onChangeText={value =>  setGameCode(value)}
+            />
+
+            <Link to='/howtoplay'>
+              <Text>Go!</Text>
+            </Link>
+
+            {  console.log('GAME CODE: ', gameCode)}
+            
+
+
             <Pressable
             style={styles.openButton}
             onPress={() => props.setModalVisible(null)}
