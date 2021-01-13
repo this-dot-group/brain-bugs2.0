c2ec877448d8f0566e3dadfe3077edf652189a0d
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2021-01-13 15:47:19 -0800

    score being sent from client to server
    Co-authored-by: Tia Low <lowtia@gmail.com>
    Co-authored-by: Josh Williams <joshuasrwilliams@gmail.com>

--- src/store/gameInfoReducer.js
+++ src/store/gameInfoReducer.js
@@ -10,8 +10,8 @@
 import axios from 'axios';
 
 // const EXPO_LOCAL_URL = '10.0.0.200'; // Josh
-const EXPO_LOCAL_URL = '192.168.0.55'; // Tia
-// const EXPO_LOCAL_URL = '10.0.0.199'; // Chris 
+// const EXPO_LOCAL_URL = '192.168.0.55'; // Tia
+const EXPO_LOCAL_URL = '10.0.0.199'; // Chris 
 
 export default (state = {}, action) => {
 
