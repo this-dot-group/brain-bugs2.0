43087c4ec600b6951959becc668a29f5452cd4ad
Joshua Williams <joshuawilliamsguitar@gmail.com>
2021-11-02 14:42:55 -0700

    Add keyboard avoiding options

--- src/components/Chat/KeyboardAvoiding.js
+++ src/components/Chat/KeyboardAvoiding.js
@@ -4,7 +4,11 @@ import { KeyboardAvoidingView, StyleSheet, Platform } from 'react-native';
 const KeyboardAvoidingComponent = ({ children }) => {
   return (
     <KeyboardAvoidingView
+      // try these options for android
       behavior={Platform.OS === 'ios' ? 'position' : ''}
+      // behavior={Platform.OS === 'ios' ? 'position' : 'height'}
+      // behavior={Platform.OS === 'ios' ? 'position' : 'position'}
+      // behavior={Platform.OS === 'ios' ? 'position' : 'padding'}
       style={styles.container}
     >
       {children}
