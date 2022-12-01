f5a0ea16329c524d9a3f9b74315ab74bc669515c
Tia Low <lowtia@gmail.com>
2022-11-30 21:16:04 -0800

    Used organized styles, start props destructure

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -4,24 +4,25 @@ import { Image } from 'react-native-elements'
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-native';
 import socketIO from 'socket.io-client';
+
 import LoadingScreen from '../LoadingScreen/LoadingScreen';
 import HowToPlayModal from '../HowToPlayModal/HowToPlayModal.js';
 import MuteButton from '../MuteButton/MuteButton';
 import { PixelButton, KeyboardAvoidingComponent, Hider } from '../Shared';
+
 import { Views } from '../../styles/';
-import { Typography } from '../../styles/'
+import { Typography } from '../../styles/';
+import { Images } from '../../styles/';
+
 import { newSocket } from '../../store/socketReducer.js';
 import { newFakeOpponent } from '../../store/fakeOpponentSocketReducer';
 import { playSound } from '../../store/soundsReducer';
 import { newUsername, newGameCode, newSocketId, newToken, deviceWidth } from '../../store/userReducer.js';
-import { EXPO_LOCAL_URL } from '../../../env'
 
+import { EXPO_LOCAL_URL } from '../../../env'
 
 const socket = socketIO(`http://${EXPO_LOCAL_URL}:3000`);
 const fakeOpponentSocket = socketIO(`http://${EXPO_LOCAL_URL}:3000`);
-
-
-
 
 function Homescreen(props) {
   const [modalVisible, setModalVisible] = useState(false)
@@ -29,43 +30,42 @@ function Homescreen(props) {
   const [toLobby, setToLobby] = useState(false);
   const [ready, setReady] = useState(false);
   const { width, height } = Dimensions.get('window');  
-  console.log('width in HomeScreen:', width)
+  const { deviceWidth, newSocket, newSocketId, newFakeOpponent  } = props;
+
+  useEffect(() => {
+    // set user device width in global state so we can use for responsive styling in other components
+    if(width < 700) deviceWidth("small")
+    if(width >= 700 && width <= 900) deviceWidth("medium")
+    if(width > 900) deviceWidth("large")
+    newSocket(socket)
+    // For some reason we need to set the socket id in two different ways
+    newSocketId(socket.id)
+    newFakeOpponent(fakeOpponentSocket);
+    props.newToken();
+    setReady(true);
+    socket.on('shareId', setSocketId);
+    return () => {
+      socket.off('shareId', setSocketId)
+    }
+  }, [])
 
   const styles = StyleSheet.create({
     // CONTAINER VIEW
     container: {
-      flex: 1,
-      paddingTop: 30,
-      paddingBottom: 30,
-      width: '100%',
-      alignItems: 'center',
-      justifyContent: 'space-between',
+      ...Views.homeScreenContainer
     },
-    logoImg: (width) => {
-      if(width < 700) {
-        return {
-          height: 80,
-          width: 80,
-        }
-      }
-      if(width >= 700 && width <= 900) {
-        return {
-          height: 100,
-          width: 100
-        }
-      }
-      if(width > 900) {
-        return {
-          height: 100,
-          width: 100
-        }
-      }
+    logoImg: {
+      ...Images.logoImg[props.screenDeviceWidth]
     },
-    // NAME TEXT
-    logoText: (width) => {
-      if(width < 700) return Typography.smHeadingText
-      if(width >= 700 && width <= 900) return Typography.mdHeadingText
-      if(width > 900) return Typography.lgHeadingText
+    logoText: {
+      ...Typography.headingText[props.screenDeviceWidth]
+    },
+    // USERNAME INPUT
+    input: {
+      ...Typography.inputText[props.screenDeviceWidth]
+    },
+    innerText: {
+      ...Typography.innerText[props.screenDeviceWidth]
     },
     // WRAPS LOGO IMG AND TEXT
     logoTextRowView: {
@@ -85,41 +85,13 @@ function Homescreen(props) {
       alignItems: 'center',
       justifyContent: 'space-between',
     },
-    // USERNAME INPUT
-    input: (width) => {
-      if(width < 700) return Typography.smInputText
-      if(width >= 700 && width <= 900) return Typography.mdInputText
-      if(width > 900) return Typography.lgInputText
-    },
-    innerText: (width) => {
-      if(width < 700) return Typography.smInnerText
-      if(width >= 700 && width <= 900) return Typography.mdInnerText
-      if(width > 900) return Typography.lgInnerText
-    }
   })
-
-
-  useEffect(() => {
-    // set user device width in state so we can use for responsive font sizing
-    props.deviceWidth(width)
-    props.newSocket(socket)
-    // For some reason we need to set the socket id in two different ways
-    props.newSocketId(socket.id)
-    props.newFakeOpponent(fakeOpponentSocket);
-    props.newToken();
-    setReady(true);
-    socket.on('shareId', setSocketId);
-    return () => {
-      socket.off('shareId', setSocketId)
-    }
-  }, [])
 
   const setSocketId = id => {
     props.newSocketId(id)
   }
 
   const handleUsernameChange = (username) => {
-
     if (username) {
       props.newUsername(username)
       setValidUsername(true);
@@ -145,8 +117,8 @@ function Homescreen(props) {
       <View style={styles.logoTextRowView}>
         <Image
           source={require('../../images/BRAIN_BUG1.png')}
-          style={styles.logoImg(props.screenDeviceWidth)} />
-        <Text style={styles.logoText(props.screenDeviceWidth)}>BRAIN BUGS</Text> 
+          style={styles.logoImg} />
+        <Text style={styles.logoText}>BRAIN BUGS</Text> 
 
         <KeyboardAvoidingComponent
           offset={0}
@@ -156,7 +128,7 @@ function Homescreen(props) {
 
             <PixelButton buttonStyle={{width: 300, flexDirection: 'row'}}>
               <TextInput
-                style={styles.input(props.screenDeviceWidth)}
+                style={styles.input}
                 placeholder={'Enter username'}
                 maxLength={15}
                 onChangeText={value => handleUsernameChange(value)}
@@ -172,10 +144,6 @@ function Homescreen(props) {
         </KeyboardAvoidingComponent>
       </View>
       
-       
-
-    
-
       <View style={styles.bottomNestedRowView}>
         <PixelButton buttonStyle={{ width: 100}}>
           <Pressable
@@ -185,7 +153,7 @@ function Homescreen(props) {
             }}
             style={{height: '100%', width: '100%'}}
           >
-            <Text style={styles.innerText(props.screenDeviceWidth)}>How To Play</Text>
+            <Text style={styles.innerText}>How To Play</Text>
           </Pressable>
         </PixelButton>
 
@@ -200,10 +168,7 @@ function Homescreen(props) {
      
       </View>
 
-      
-
       {toLobby && <Redirect to='/lobby' />}
-
       
     </View>
   )
