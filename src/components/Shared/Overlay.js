e5e3d4f4eedfe110411f56b356b8f2baab81fbd4
Joshua Williams <joshuawilliamsguitar@gmail.com>
2023-02-15 17:23:33 -0800

    Fix loop bug

--- src/components/Shared/Overlay.js
+++ src/components/Shared/Overlay.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Pressable, StyleSheet } from 'react-native';
-import { Hider } from '.';
+import Hider from './Hider';
 
 export default function Overlay ({
   active,
